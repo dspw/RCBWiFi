61ff8d25a1d2a0dbf78e658c1a036bf315b7a9c1
dspw <3029861+dspw@users.noreply.github.com>
2024-08-13 15:58:47 -0400

    version 0.1.1

--- Source/RcbWifi.h
+++ Source/RcbWifi.h
@@ -49,7 +49,7 @@ const int BATT_STREAM_THRESH = 3.6;
 const bool FACTORY_TEST_MODE = 1;
 
 // Plugin Version
-const String PLUGIN_VERSION = "v0.1.0";
+const String PLUGIN_VERSION = "v0.1.1";
 
 //#define CHIP_ID_RHD2132  1
 //#define CHIP_ID_RHD2216  2
